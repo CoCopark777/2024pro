59e3a3ed2af24275812ccbde346b498840c0ce2a
CoCopark777 <825510546@qq.com>
2024-09-30 00:49:43 +0800

    增加流程文件

---
+++ api/contract.py
@@ -0,0 +1,27 @@
+# 合同上传
+
+"""
+URL： http://kdtx-test.itheima.net/api/common/upload
+方法：Post
+请求数据：
+请求头{"Content-Type": "multipart/from-data", "Authorization": "xxx"}
+请求体:{"file": "合同文件"}
+
+"""
+
+import requests
+
+
+class ContractAPI:
+    # 初始化
+    def __init__(self):
+        self.url_upload = "http://kdtx-test.itheima.net/api/common/upload"
+        self.url_add_contract = "http://kdtx-test.itheima.net/api/contract"
+
+    # 合同上传接口
+    def upload_contract(self, test_data, token):
+        return requests.post(url=self.url_upload, files={"file": test_data}, headers={"Authorization": token})
+
+    # 合同新增接口
+    def add_contract(self, test_data, token):
+        return requests.post(url=self.url_add_contract, json=test_data, headers={"Authorization": token})
--- script/test03_contract_busniess.py
+++ script/test03_contract_busniess.py
@@ -1,19 +1,23 @@
 # 导包
 from api.login import LoginAPI
 from api.course import CourseAPI
+from api.contract import ContractAPI
 
 # 创建测试类
 class TestContractBusiness:
     token = None
-    #前置处理
+
+    # 前置处理
     def setup_method(self):
         # 实例化接口对象
         self.login_api = LoginAPI()
         self.course_api = CourseAPI()
+        self.contract_api = ContractAPI()
 
-    #后置处理
+    # 后置处理
     def teardown_method(self):
         pass
+
     # 1.登录成功
     def test01_login_success(self):
         # 获取验证码
@@ -35,6 +39,27 @@ class TestContractBusiness:
 
     # 2.课程新增成功
     def test02_add_course(self):
-        add_data = {"name":"测试开发课", "subject":"6", "price":899, "applicablePerson":"2", "info":"测试开发课"}
+        add_data = {"name": "测试开发课", "subject": "6", "price": 899, "applicablePerson": "2", "info": "测试开发课"}
         response = self.course_api.add_course(test_data=add_data, token=TestContractBusiness.token)
+        print(response.json())
+
+    # 3.合同上传成功
+    def test03_upload_contract(self):
+        f = open("../data/zyf.webp", "rb")
+        response = self.contract_api.upload_contract(test_data="", token=TestContractBusiness.token)
+        print(response.json())
+
+    # 4.合同新增
+    def test04_add_contract(self):
+        test_data = {
+            "name": "测试202409",
+            "phone": "12345789",
+            "contractNo": "HT20240930",
+            "subject": "6",
+            "courseId": "99",
+            "channel": "0",
+            "activityId": 77,
+            "fileName": "xxx"
+        }
+        response = self.contract_api.add_contract(test_data=test_data, token=TestContractBusiness.token)
         print(response.json())
